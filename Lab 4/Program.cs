0ba222b840362c6b390874fe5a5dfc46fbedbd31
veram <veram@VERACER>
2022-07-26 01:24:30 +0300

    Add project files.

---
+++ Lab 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int d = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                d += 2*i - 1;
+                Console.WriteLine(d);
+            }
+            Console.WriteLine("Результат равен {0}",d);
+            Console.ReadKey();
+        }
+    }
+}
